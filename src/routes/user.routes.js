443c4e5b6c6c60a744739c08c2c70eed2767b5f1
vinay1945 <vinaykumarporandla19@gmail.com>
2024-12-14 16:39:01 +0530

    all file added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -11,7 +11,7 @@ import {
   updateUserCoverImage,
   getUserChannelProfile,
   getWatchHistory,
-} from "../controllers/user.controllers.js";
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
